3f19cc4539fff05c383957d82bf467e86b42f18f
Jeromy Condon <jeromy@rufusmedia.com>
2015-01-05 15:36:04 -0800

    moved html to index file.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- meta desc & title will be supplied by plugin -->
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- stylesheets & stuff -->
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/site-assets/favicon.ico">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/style.min.css">
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
+
+    
+
+    <?php wp_footer(); ?>
+  </body>
+
+</html>
